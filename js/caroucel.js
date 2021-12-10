8d302bcf87a9f1f38cf97a72eb7df246d7b89e84
royalguard14 <95269746+royalguard14@users.noreply.github.com>
2021-12-10 23:12:36 +0800

    Update caroucel.js

--- js/caroucel.js
+++ js/caroucel.js
@@ -12,8 +12,7 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-
-
+/*
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -24,4 +23,22 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+*/
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
